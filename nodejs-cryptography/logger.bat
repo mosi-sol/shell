00d6783b3a691ba6e4199886c882726211def52f
mosi <99150661+mosi-sol@users.noreply.github.com>
2023-03-12 11:53:00 +0330

    Update logger.bat

--- nodejs-cryptography/logger.bat
+++ nodejs-cryptography/logger.bat
@@ -3,6 +3,7 @@ color 0A
 title crypto graphy - https://github.com/mosi-sol/shell/nodejs-cryptography/
 ::cryptography usecase
 
+echo look at result.txt for data
 node index.js > result.txt
 
 pause
