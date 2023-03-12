e6039474882bfb9269c6e0c96c0df1c393695075
mosi <99150661+mosi-sol@users.noreply.github.com>
2023-03-12 11:52:08 +0330

    Update logger.bat

--- nodejs-cryptography/minimal/logger.bat
+++ nodejs-cryptography/minimal/logger.bat
@@ -1,5 +1,6 @@
 @echo off
 color 0A
+title crypto graphy - https://github.com/mosi-sol/shell/nodejs-cryptography/
 ::expermential project
 echo hash generator , look at: minimal.txt
 node index.js > minimal.txt
