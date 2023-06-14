b0d9d054802fee2432a72883f34f18140f3d22bb
mosi <99150661+mosi-sol@users.noreply.github.com>
2023-06-14 03:31:01 +0330

    Create Bitwise.sol

---
+++ BitwiseLib/Bitwise.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+library Bitwise {
+
+    function and(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a & b;
+    }
+
+    function or(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a | b;
+    }
+
+    function xor(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a ^ b;
+    }
+
+    function not(uint256 a) internal pure returns (uint256) {
+        return ~a;
+    }
+
+    function mask(uint256 a, uint256 mask) internal pure returns (uint256) {
+        return a & mask;
+    }
+}
